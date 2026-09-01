7922a06ff4549936b43b9738567f2b6ab3bf536b
ditopsen <estebangzcs@gmail.com>
2026-09-01 07:39:17 -0500

    Update conexion.php

--- login_project/config/conexion.php
+++ login_project/config/conexion.php
@@ -12,7 +12,7 @@ class Conexion {
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDo::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            die("Error en la conexion: " . $e->getMesage());
+            die("Error en la conexion: " . $e->getMessage());
         }
 
     }
