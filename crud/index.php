e3559770a08e33da88d754ae92f04fdabb63d0f6
KyotoAt <jhonatanunistudi@gmail.com>
2026-09-01 09:55:16 -0500

    1899

--- crud/index.php
+++ crud/index.php
@@ -5,38 +5,50 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Crud php y MySQL</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" 
-	integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 	<script src="https://kit.fontawesome.com/edf14e2fc0.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-	<h1 class="text-center p-3">Hola, bienvenido</h1>
+	<script>
+		function eliminar() {
+			var respuesta = confirm("¿Estas seguro de querer eliminar el registro?");
+			return respuesta
+		}
+	</script>
+	<h1 class="text-center p-3">Modulo de Registro de usuarios</h1>
+	<?php
+	include "modelos/conexion.php";
+	include "controlador/eliminar_persona.php";
+	?>
 	<div class="container-fluid row">
-		<form class="col-4" p-3>
+		<form class="col-4 p-3" method="POST">
 			<h3 class="text-center text-secondary">Registro de personas</h3>
+			<?php
+			include "controller/resgistro_persona.php";
+			?>
 			<div class="mb-3">
-				<label for="exampleInputEmail1" class="form-label">Nombre</label>
+				<label for="exampleInputEmail1" class="form-label">Nombre:</label>
 				<input type="text" class="form-control" name="nombre">
 			</div>
 			<div class="mb-3">
-				<label for="exampleInputEmail1" class="form-label">Apellido</label>
+				<label for="exampleInputEmail1" class="form-label">Apellido:</label>
 				<input type="text" class="form-control" name="apellido">
 			</div>
 			<div class="mb-3">
-				<label for="exampleInputEmail1" class="form-label">Documento</label>
+				<label for="exampleInputEmail1" class="form-label">Documento:</label>
 				<input type="text" class="form-control" name="documento">
 			</div>
 			<div class="mb-3">
-				<label for="exampleInputEmail1" class="form-label">fecha de nacimiento</label>
+				<label for="exampleInputEmail1" class="form-label">Fecha de Nacimiento:</label>
 				<input type="date" class="form-control" name="fecha">
 			</div>
 			<div class="mb-3">
-				<label for="exampleInputEmail1" class="form-label">correo</label>
+				<label for="exampleInputEmail1" class="form-label">Correo:</label>
 				<input type="text" class="form-control" name="correo">
 			</div>
-
-			<button type="submit" class="btn btn0-primary/" name="btnregistrar" value="ok">Registrar</button>
+			<button type="submit" class="btn btn-primary" name="btnregistrar" value="ok">Registrar</button>
 		</form>
 		<div class="col-8 p-4">
 			<table class="table">
@@ -46,33 +58,37 @@
 						<th scope="col">Nombre</th>
 						<th scope="col">Apellido</th>
 						<th scope="col">Documento</th>
-					    <th scope="col">Fecha de nacimiento</th>
-				        <th scope="col">Correo</th>
+						<th scope="col">Fecha de Nacimiento</th>
+						<th scope="col">Correo</th>
 						<th scope="col"></th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-						<td>
-							<a herf="" class="btn btn-small btn" herf="<i class="fa-solid fa-pen-fancy"></i></a>																				
-							<a herf="<i class="fa-solid fa-trash"></i></a>	
-						</td>
-					</tr>
-					
+					<?php
+					include "modelos/conexion.php";
+					$sql = $conexion->query("SELECT * FROM tb_persona");
+					while ($datos = $sql->fetch_object()) { ?>
+						<tr>
+							<td><?= $datos->id ?></td>
+							<td><?= $datos->nombre ?></td>
+							<td><?= $datos->apellido ?></td>
+							<td><?= $datos->documento ?></td>
+							<td><?= $datos->fecha_nac ?></td>
+							<td><?= $datos->correo ?></td>
+							<td>
+								<a href="modificar_persona.php?id=<?= $datos->id ?>" class="btn btn-small btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
+								<a onclick="return eliminar()" href="index.php?id=<?= $datos->id ?>" class="btn btn-small btn-danger"><i class="fa-solid fa-trash-can"></i></a>
+							</td>
+						</tr>
+					<?php }
+					?>
 				</tbody>
 			</table>
 		</div>
 	</div>
-
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
-	integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" 
-	crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+		crossorigin="anonymous"></script>
 </body>
 
 </html>
